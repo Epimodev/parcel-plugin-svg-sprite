ecbc3a53a7fc198704025449840165e86a3b64b6
Epimodev <godin.fred8@gmail.com>
2019-05-27 19:31:44 +0200

    Fix assets file to handle raw import from js

--- src/asset.js
+++ src/asset.js
@@ -1,7 +1,28 @@
 const { Asset } = require('parcel-bundler');
+const urlJoin = require('parcel-bundler/lib/utils/urlJoin');
+const md5 = require('parcel-bundler/lib/utils/md5');
 const { isPathIncluded, createHash } = require('./utils');
 
 class SvgAsset extends Asset {
+  /**
+   * @desc check if we use raw asset behavior or svg sprite behavior
+   */
+  useRawAssetsBehavior() {
+    return !isPathIncluded(this.name);
+  }
+
+  /**
+   * @desc load asset contents
+   */
+  // eslint-disable-next-line consistent-return
+  load() {
+    if (this.useRawAssetsBehavior()) {
+      // we do nothing because it will be copied by the RawPackager directly.
+    } else {
+      return super.load();
+    }
+  }
+
   /**
    * @desc Generate asset of an svg file imported by js bundle
    *
@@ -11,9 +32,17 @@ class SvgAsset extends Asset {
   async generate() {
     const hash = await this.generateHash();
 
-    // if path isn't include, we keep original behavior with RawAssets
-    if (!isPathIncluded(this.name)) {
-      return {};
+    // if path isn't include, we keep original RawAsset behavior
+    if (this.useRawAssetsBehavior()) {
+      // code copied from RawAsset to copy RawAsset behavior
+      // https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/assets/RawAsset.js
+      const pathToAsset = urlJoin(this.options.publicURL, this.generateBundleName());
+      return [
+        {
+          type: 'js',
+          value: `module.exports=${JSON.stringify(pathToAsset)};`,
+        },
+      ];
     }
 
     return [
@@ -27,7 +56,7 @@ class SvgAsset extends Asset {
       },
       {
         type: 'js',
-        value: `module.exports = '#${hash}'`,
+        value: `module.exports='#${hash}'`,
       },
     ];
   }
@@ -37,7 +66,11 @@ class SvgAsset extends Asset {
    * @return {string}
    */
   async generateHash() {
-    return createHash(this.contents).toString();
+    if (this.content) {
+      return createHash(this.contents).toString();
+    }
+
+    return md5.file(this.name);
   }
 }
 
